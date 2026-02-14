e71a0700e98e976880efbc06e3b8d3d3f140a83e
Esraa <essomostafa57@gmail.com>
2026-02-14 19:54:12 +0200

    first commit

--- script.js
+++
@@ -1,7 +0,0 @@
- const links = document.querySelectorAll("li");
-links.forEach(link => {
-    link.addEventListener("click" , () => {
- links.forEach(i => i.classList.remove("active"));
-link.classList.add("active"); 
-});
-});
